0e9b131957302464596459e5d59a773147ec251e
Khiem Doan <doankhiem.crazy@gmail.com>
2025-08-19 10:55:46 +0700

    Revert "Update compose for easier setup"

--- mkdirs.sh
+++ mkdirs.sh
@@ -7,12 +7,8 @@ sudo chown 1000:1000 -R .data
 
 # MinIO
 mkdir -p .data/minio
-sudo chown 1000:1000 -R .data/minio
+sudo chown 0:0 -R .data/minio
 
-# StarRocks FE
-mkdir -p .data/starrocks-fe
-sudo chown 1000:1000 -R .data/starrocks-fe
-
-# StarRocks CN
-mkdir -p .data/starrocks-cn
-sudo chown 1000:1000 -R .data/starrocks-cn
+# StarRocks
+mkdir -p .data/starrocks
+sudo chown 0:0 -R .data/starrocks
